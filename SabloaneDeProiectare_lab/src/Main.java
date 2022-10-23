ff1368d4a78bb5056273be405fdca68fde596340
CosminaLaviniaDutca <cosmina.dutca02@e-uvt.ro>
2022-10-23 15:05:17 +0300

    Lab2(interface)

--- SabloaneDeProiectare_lab/src/Main.java
+++ SabloaneDeProiectare_lab/src/Main.java
@@ -10,13 +10,13 @@ public class Main {
         int indexSubChapterOneOne = chp1.createSubChapter( "Subcapitolul 1.1" );
         SubChapter scOneOne = chp1.getSubChapter(indexSubChapterOneOne);
 
-        scOneOne.createNewParagraph("Paragraph 1");
-        scOneOne.createNewParagraph("Paragraph 2");
-        scOneOne.createNewParagraph("Paragraph 3");
-        scOneOne.createNewImage("Image 1");
-        scOneOne.createNewParagraph("Paragraph 4");
-        scOneOne.createNewTable("Table 1");
-        scOneOne.createNewParagraph("Paragraph 5");
+        scOneOne.addElement(new Paragraph("Paragraph 1"));
+        scOneOne.addElement(new Paragraph("Paragraph 2"));
+        scOneOne.addElement(new Paragraph("Paragraph 3"));
+        scOneOne.addElement(new Image("Image 1"));
+        scOneOne.addElement(new Paragraph("Paragraph 4"));
+        scOneOne.addElement(new Table("Table 1"));
+        scOneOne.addElement(new Paragraph("Paragraph 5"));
         scOneOne.print();
     }
 }
